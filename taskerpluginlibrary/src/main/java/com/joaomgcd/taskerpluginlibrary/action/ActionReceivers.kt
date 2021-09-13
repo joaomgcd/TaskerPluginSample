a816dc704dad056ec10f44b12025bad71c9cbbba
Jonah Hirsch <jonahwh@gmail.com>
2021-09-12 18:09:28 -0700

    Add ability for Actions to be Cancelled
    By sending an Intent such as:
    ```
    Intent(context, IntentServiceAction::class.java).apply {
      action = IntentServiceAction.ACTION_STOP
    }
    ```
    
    Any running actions will be stopped with an InterruptedException

--- taskerpluginlibrary/src/main/java/com/joaomgcd/taskerpluginlibrary/action/ActionReceivers.kt
+++ taskerpluginlibrary/src/main/java/com/joaomgcd/taskerpluginlibrary/action/ActionReceivers.kt
@@ -3,8 +3,11 @@ package com.joaomgcd.taskerpluginlibrary.action
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.joaomgcd.taskerpluginlibrary.R
 import com.joaomgcd.taskerpluginlibrary.extensions.runFromTasker
+import com.joaomgcd.taskerpluginlibrary.runner.ArgsSignalFinish
 import com.joaomgcd.taskerpluginlibrary.runner.IntentServiceParallel
+import com.joaomgcd.taskerpluginlibrary.runner.TaskerPluginResultError
 import net.dinglisch.android.tasker.TaskerPlugin
 
 
@@ -20,11 +23,31 @@ class BroadcastReceiverAction : BroadcastReceiver() {
 }
 
 class IntentServiceAction : IntentServiceParallel("IntentServiceTaskerAction") {
+    private var taskerIntent: Intent? = null
+
     override fun onHandleIntent(intent: Intent) {
         startForegroundIfNeeded()
+        taskerIntent = intent
         val result = TaskerPluginRunnerAction.runFromIntent(this, intent)
         if (!result.hasStartedForeground) {
             startForegroundIfNeeded()
         }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent?.action.equals(ACTION_STOP)) {
+            stopSelf()
+            taskerIntent?.let {
+                TaskerPluginResultError(
+                        InterruptedException(getString(R.string.cancelled))
+                ).signalFinish(ArgsSignalFinish(this, it))
+            }
+            return START_NOT_STICKY
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    companion object {
+        const val ACTION_STOP = "ACTION_STOP"
     }
 }
